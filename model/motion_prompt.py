add gradient checkpoint. fix require_grad

model/motion_prompt.py
@@ -6,14 +6,14 @@ from einops import rearrange
 class ResidualCrossAttentionBlock(ResidualAttentionBlock):
     def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None):
         super().__init__(d_model, n_head, attn_mask)
-        self.ln_1_y = LayerNorm(d_model)
+        #self.ln_1_y = LayerNorm(d_model)
     
-    def attention(self, x: torch.Tensor, y: torch.Tensor):
+    def attention(self, x: torch.Tensor, y: torch.Tensor=None):
         self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
-        return self.attn(y, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
+        return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
 
-    def forward(self, x: torch.Tensor, y: torch.Tensor):
-        x = x + self.attention(self.ln_1(x),self.ln_1_y(y))
+    def forward(self, x: torch.Tensor, y: torch.Tensor=None):
+        x = x + self.attention(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
 
@@ -24,7 +24,7 @@ class CrossTransformer(Transformer):
     
     def forward(self, x: torch.Tensor, y: list):
         for blk, y_ in zip(self.resblocks, y[:len(self.resblocks)]):
-            x = blk(x, y_)
+            x = torch.utils.checkpoint.checkpoint(blk,x)
         return x
 
 class MotionPrompt(VisionTransformer):
@@ -62,15 +62,3 @@ class MotionPrompt(VisionTransformer):
             x = x @ self.proj
 
         return x
-
-# class MotionPrompt(nn.Module):
-#     def __init__(self, n_trunk:int=8, out_features:int=2048, thres:float=4.0) -> None:
-#         super().__init__()
-
-#         self.frame_diff = Sandevistan(n_trunks=n_trunk, thres=thres)
-    
-#     def forward(self, x):
-#         with torch.no_grad():
-#             # B T C H W
-#             x = self.frame_diff(x)
-#         return x

model/network.py
@@ -5,7 +5,6 @@ from clip.model import CLIP, ResidualAttentionBlock, VisionTransformer, convert_
 from util.frame_diff import Sandevistan
 from einops import rearrange
 from model.motion_prompt import MotionPrompt, ResidualCrossAttentionBlock
-import warnings
 
 
 class Transformer_(nn.Module):
@@ -150,7 +149,7 @@ class SandevistanCLIP(CLIP):
         frames = rearrange(frames, 'b t c h w -> (b t) c h w').detach()
         class_features, frame_features = self.visual(frames.type(self.dtype))
         motion_features = self.encode_motion(
-            motion_feat.type(self.dtype), frame_features)
+            motion_feat.type(self.dtype).requires_grad_(True), frame_features)
 
         #del frame_features
         class_features = class_features.view(

run.py
@@ -17,7 +17,7 @@ from einops import rearrange
 import time
 from tqdm import tqdm, trange
 from torch.utils.tensorboard import SummaryWriter
-
+import gc
 
 def load_model():
     if cfg.resume is not None:
@@ -32,7 +32,7 @@ def load_model():
             T=cfg.data.seg_length,
             thres=cfg.network.motion.thres,
             alpha=cfg.network.alpha.value
-        ).train().cuda()
+        ).cuda()
         optimizer = get_optimizer(cfg, model)
         optimizer.load_state_dict(state_dict['optimizer_state_dict'])
     elif cfg.pretrain is not None:
@@ -61,7 +61,6 @@ def load_model():
             rearrange(text_tokenized, 'c n d -> (c n) d').cuda())
         all_text_features = rearrange(
             all_text_features, '(c n) d -> c n d', n=num_text_aug)
-
     return model, optimizer, lr_scheduler, start_epoch, train_dataloader, validate_dataloader, num_text_aug, text_tokenized, all_text_features
 
 
@@ -102,7 +101,7 @@ def train():
                 # Make ground Truth (confussion matrix)
                 label = data['label'].unsqueeze(-1)
                 ground_truth = (0.94+0.05*torch.randn(label.shape[0]))*(torch.eq(label, label.T).to(torch.float16))
-                ground_truth += torch.rand_like(ground_truth)*0.01
+                #ground_truth += torch.rand_like(ground_truth)*0.01
 
                 loss_img = criterion_img(logits_per_image, ground_truth.cuda())
                 loss_txt = criterion_txt(logits_per_text, ground_truth.cuda())
@@ -133,6 +132,7 @@ def train():
                     torch.cuda.empty_cache()
                     prec1 = eval(epoch)
                     torch.cuda.empty_cache()
+                    gc.collect()
                 is_best = prec1 > best_prec1
                 best_prec1 = max(prec1, best_prec1)
                 print('Testing: {}/{}'.format(prec1, best_prec1))
@@ -203,7 +203,8 @@ if __name__ == '__main__':
     writer = SummaryWriter(working_dir)
 
     model, optimizer, lr_scheduler, start_epoch, train_dataloader, validate_dataloader, num_text_aug, text_tokenized, all_text_features = load_model()
-
+    gc.collect()
+    torch.cuda.empty_cache()
     if args.train:
         if cfg.optim.distributed:
             torch.distributed.init_process_group(backend='nccl')