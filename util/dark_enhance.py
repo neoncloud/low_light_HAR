58ea21251ffc8701b70d82c8124a0902c0820dc0
neoncloud <1840652@brunel.ac.uk>
2022-11-04 10:18:11 +0800

    Clean up eval() routine

--- util/dark_enhance.py
+++ util/dark_enhance.py
@@ -3,39 +3,42 @@ import torch.nn.functional as F
 
 
 class DarkEnhance(torch.nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, patch_size: int = 7, pad_size: int = 3, omega: float = 0.98, t0: float = 0.98) -> None:
         super().__init__()
+        self.omega = omega
+        self.t0 = torch.tensor([t0])
+        self.pad = torch.nn.ConstantPad2d(
+            (pad_size, pad_size, pad_size, pad_size), 255.0)
+        self.max_filter = torch.nn.MaxPool2d(kernel_size=patch_size, stride=1)
 
     def get_dark_channel(self, image: torch.Tensor):
-        patchSize = 7
-        padSize = 3
         jDark = image.min(-1).values
-        img = F.pad(jDark, ((padSize, padSize, padSize, padSize)),
-                    mode='constant', value=255)
-        jDark = -F.max_pool2d(-img, kernel_size=patchSize, stride=1)
+        img = self.pad(jDark)
+        jDark = -self.max_filter(-img)
         return jDark
 
     def estimate_atmospheric_light(self, image: torch.Tensor, jDark: torch.Tensor):
         height, width = jDark.shape[1], jDark.shape[2]
-        numpx = max(width * height / 1000, 1)
+        numpx = max(width * height // 1000, 1)
         jDarkVec = jDark.flatten(1, 2)
         imgVec = image.flatten(1, 2)
-        _, indices = torch.sort(jDarkVec, stable=True)
+        #_, indices = torch.sort(jDarkVec, stable=True)
         #A = imgVec[torch.arange(imgVec.shape[0]).unsqueeze(-1),indices[:,int(width * height - numpx):-1]].mean(1)
-        A = torch.gather(imgVec, 1, indices[:, int(
-            width * height - numpx):-1, None]).mean(1)
+        # A = torch.gather(imgVec, 1, indices[:, int(
+        #     width * height - numpx):-1, None]).mean(1)
+        _, indices = torch.topk(jDarkVec, k=int(
+            width * height - numpx), dim=1, largest=False)
+        A = torch.gather(imgVec, 1, indices[:, :, None]).mean(1)
         return A[:, None, None, :]
 
     def estimate_transmission(self, image: torch.Tensor, A: torch.Tensor):
-        omega = 0.98
-        img = image/A
-        transmission = 1 - omega * self.get_dark_channel(img)
+        image /= A
+        transmission = 1 - self.omega * self.get_dark_channel(image)
         return transmission
 
     def dehaze(self, image: torch.Tensor, A: torch.Tensor, transmission: torch.Tensor):
-        t0 = torch.tensor([0.15], device=transmission.device)
-        J = A + (image - A)/torch.maximum(transmission, t0).unsqueeze(-1)
-        return J
+        return A + (image - A)/torch.maximum(transmission, self.t0).unsqueeze(-1)
+        
 
     def forward(self, HazeImg: torch.Tensor):
         HazeImg_ = 255.0-HazeImg
